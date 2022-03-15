aaca10549226c398edbbacfd0f2b168bd7699bc4
Steve Alomia <stevealomia@yahoo.com>
2022-03-15 11:19:06 -0400

    commit tree 2da4d161c8d62b7a13c7af204333033bb18b0f33

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1= 31;
+const num2=2;
+const multiply= num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()+2);
+
+const num3= 14;
+const num4= 10;
+const mod = num3 % num4;
+
+const max = (5,9,15,20);
+Math.max();
